29389b08ec599c79895fd8ffc0622afcbaf38aa4
Nguyễn Lê Duy <147262594+NguyenLeDuyy@users.noreply.github.com>
2025-05-24 13:48:14 +0700

    use google font

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,16 +1,24 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Roboto } from "next/font/google";
 import "./globals.css";
+import localFont from 'next/font/local'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const roboto = Roboto({ subsets: ['vietnamese'], weight: ['100', '300'] })
+
+const myFont = localFont({
+  src: [{
+    path: './Roboto-Thin.ttf',
+    weight: '100'
+  },
+  {
+    path: './Roboto-Regular.ttf',
+    weight: '400'
+  }
+  ],
+  display: "swap",
+  variable: '--font-roboto',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,13 +32,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${myFont.variable}`}
       >
         {children}
       </body>
