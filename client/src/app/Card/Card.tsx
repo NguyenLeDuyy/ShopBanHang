68beedd86b9373a57b7815b2f5c6f740b8661718
Nguyễn Lê Duy <147262594+NguyenLeDuyy@users.noreply.github.com>
2025-05-24 09:46:01 +0700

    css style trong nextjs

--- client/src/app/Card/Card.tsx
+++ client/src/app/Card/Card.tsx
@@ -1,16 +1,19 @@
 'use client'
 
+import { useState } from 'react';
 import './card.css';
 import custom from './custom.module.scss';
+import clsx from 'clsx'
 
 export default function Card() {
 
-    // const [expanding, setExpanding] = useState(false);
+    const [expanding, setExpanding] = useState(false);
 
     return (
         <div>
-            <div className="bg-red-500 text-white">Test Tailwind</div>
-            <div className={`card ${custom.card}`}>Card</div>
+            <div className={clsx('card', {
+                [custom.card]: expanding
+            })}>Card</div>
 
         </div>
     )
