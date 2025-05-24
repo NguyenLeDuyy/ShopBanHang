b3bf7735f3ed9f98f96897e51fa56dde63f8443e
Nguyễn Lê Duy <147262594+NguyenLeDuyy@users.noreply.github.com>
2025-05-24 15:33:44 +0700

    use image

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,19 @@
 // import { Button } from "@/components/ui/button";
 
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl font-sans text-center">Welcome to NextJS</h1>
+      <div className="w-[700px] h-[700px] bg-red-300">
+        <Image
+          src='https://images.pexels.com/photos/16105790/pexels-photo-16105790/free-photo-of-toa-nha-d-ng-lau-dai-l-ch-s.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+          alt="suffer"
+          width={500}
+          height={500}
+          quality={100}
+        />
+      </div>
     </main>
   );
 }
